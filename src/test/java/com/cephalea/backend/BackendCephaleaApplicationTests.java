ad6968f0f9ac79adeb8a43977d02e3aa1d688a02
Manon13380 <manon.carpentieri@hotmail.com>
2025-08-16 13:58:34 +0200

    test workflow 3

--- src/test/java/com/cephalea/backend/BackendCephaleaApplicationTests.java
+++ src/test/java/com/cephalea/backend/BackendCephaleaApplicationTests.java
@@ -1,15 +1,23 @@
 package com.cephalea.backend;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
+
+import java.util.Arrays;
 
 @SpringBootTest
 @ActiveProfiles("test")
 class BackendCephaleaApplicationTests {
 
+
+	@Autowired
+	private org.springframework.core.env.Environment env;
+
 	@Test
 	void contextLoads() {
+		System.out.println("Active profiles: " + Arrays.toString(env.getActiveProfiles()));
 	}
 
 }
