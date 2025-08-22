337b4e8c55eabb0d2d9588e3ca492275c0542e59
Manon13380 <manon.carpentieri@hotmail.com>
2025-08-22 22:26:33 +0200

    change Readme.md

--- src/main/java/com/cephalea/backend/service/PasswordResetService.java
+++ src/main/java/com/cephalea/backend/service/PasswordResetService.java
@@ -42,7 +42,7 @@ public class PasswordResetService {
 
         tokenRepository.save(resetToken);
 
-        String resetUrl = urlResetPwd + "reset-password?token=" + token;
+        String resetUrl = "http://57.128.84.212:9090/reset-password?token=" + token;
 
         String subject = "Réinitialisation de mot de passe";
 
